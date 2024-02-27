d30104a92a579b4b745d320bd44d844b33063b46
Ignat Bojinov <ignatbojinov@gmail.com>
2024-02-27 15:27:27 +0100

    create inteface

---
+++ Project/IRecipe.cs
@@ -0,0 +1,10 @@
+namespace RecipeMS
+{
+    interface IRecipe
+    {
+        public void AddRecipe();
+        public void ViewRecipe();
+        public void UpdateRecipe();
+        public void CategorizeRecipe();
+    }
+}
