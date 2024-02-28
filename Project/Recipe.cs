2a1da1cad67edffc525a2f67138cca16ceb82bc0
Francesco Schenone <frsk@Francescos-MacBook-Air.local>
2024-02-28 10:39:53 +0100

    Provided an enum for categories

--- Project/Recipe.cs
+++ Project/Recipe.cs
@@ -4,19 +4,39 @@ namespace RecipeMS
 {
     public class Recipe
     {
-        public Guid RecipeID {get; set;} = Guid.NewGuid();
+        public Guid RecipeID = Guid.NewGuid();
         public string? Title {get; set;}
         public List<string>? Ingredients {get; set;}
         public string? Instructions {get; set;}
-        public string? Category {get; set;}
+        public FoodCategory? Category {get; set;}
 
-        public Recipe(string title, List<string> ingredients, string instructions, string category)
+        public Recipe(string title, List<string> ingredients, string instructions, FoodCategory Category)
         {
             this.Title = title;
             this.Ingredients = ingredients;
             this.Instructions = instructions;
-            this.Category = category;
+            this.Category = Category;
         }
 
+        public enum FoodCategory
+        {
+            Vegan,
+            Italian,
+            Chinese,
+            High_Protein,
+            Low_Fat,
+            Low_Carb,
+            Mediterranean,
+            Gluten_Free,
+            Polish,
+            Thai,
+            Pork,
+            Chicken,
+            Eggs,
+            Cake,
+            Salad,
+            Bread,
+            Dessert
+        }
     }
 }
