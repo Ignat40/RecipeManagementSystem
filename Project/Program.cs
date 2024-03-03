84484f1ebcbafbdb790413c10d5486dde0fc5a50
Ignat Bojinov <ignatbojinov@gmail.com>
2024-03-03 20:17:42 +0100

    create main method

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,23 @@
-﻿namespace RecipeMS
+﻿using System;
+
+namespace RecipeMS
 {
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of RecipeManager with JsonRecipeStorage
+            var recipeManager = new RecipeManager(new JsonRecipeStorage());
+
+            // Add a test recipe
+            recipeManager.AddRecipe();
+
+            // Save recipes to the JSON file
+            recipeManager.Save();
+
+            // Load and display recipes
+            recipeManager.Load();
+            recipeManager.ViewRecipe();
+        }
+    }
 }
