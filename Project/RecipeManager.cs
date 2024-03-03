65470264f65cd43472b2986c2018c4bc9269e885
Francesco Schenone <frsk@Francescos-MacBook-Air.local>
2024-03-03 17:59:23 +0100

    added AddRecipe + ViewRecipe method and ui

--- Project/RecipeManager.cs
+++ Project/RecipeManager.cs
@@ -1,22 +1,88 @@
-namespace RecipeMS
+namespace RecipeMS;
+
+using static System.Console;
+
+public class RecipeManager : IRecipe
 {
-    public class RecipeManager : IRecipe
+    List<Recipe> Recipes = new();
+
+    public void AddRecipe()
     {
-        public void AddRecipe()
-        {
+        WriteLine("Input recipee name: ");
+        string? name = ReadLine();
 
-        }
-        public void ViewRecipe()
-        {
+        WriteLine("Input ingredients for the recipe separated by a comma: ");
+        string? ingredientsInput = ReadLine();
+        List<string> ingredients = ingredientsInput.Split(",").ToList();
 
-        }
-        public void UpdateRecipe()
-        {
+        WriteLine("Input instructions: ");
+        string? instructions = ReadLine();
 
-        }
-        public void CategorizeRecipe()
+        Recipe.FoodCategory category = GetFoodCategory();
+
+        Recipe recipe = new Recipe(name, ingredients, instructions, category);
+        Recipes.Add(recipe);
+        Console.WriteLine("Recipe added");
+        WriteLine(recipe.ToString());
+    }
+
+    public void ViewRecipe()
+    {
+        WriteLine("Which recipee do you want to see? ");
+        int i = 1;
+
+        foreach (var recipe in Recipes)
         {
-            
+            WriteLine(i + ": " + recipe.Title);
         }
+
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            validInput = int.TryParse(Console.ReadLine(), out int recipeNum);
+
+            if (validInput == false || recipeNum > Recipes.Count || recipeNum < 0)
+            {
+                WriteLine("Inavlid input, try again");
+            }
+
+            if (validInput == true)
+            {
+                recipeNum = recipeNum - 1;
+                WriteLine(Recipes[recipeNum].ToString());
+            }
+        }
+    }
+
+    public void UpdateRecipe()
+    {
+
+    }
+    public void CategorizeRecipe()
+    {
+
+    }
+
+    private Recipe.FoodCategory GetFoodCategory()
+    {
+        int i = 1;
+        WriteLine("Choose a category from the following");
+        foreach (string s in Enum.GetNames(typeof(Recipe.FoodCategory)))
+        {
+            WriteLine("{0}: {1}", i, s);
+            i++;
+        }
+
+        if (int.TryParse(Console.ReadLine(), out int categoryNum))
+        {
+            categoryNum = categoryNum - 1;
+            if (Enum.IsDefined(typeof(Recipe.FoodCategory), categoryNum))
+            {
+                Recipe.FoodCategory myCategory = (Recipe.FoodCategory)categoryNum;
+                return myCategory;
+            }
+        }
+        return Recipe.FoodCategory.Blank;
     }
 }
