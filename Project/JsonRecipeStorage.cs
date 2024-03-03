276a88a659c502d6d6e800df812f8d82abd93885
Ignat Bojinov <ignatbojinov@gmail.com>
2024-03-03 20:20:02 +0100

    Create Load and Save methods

--- Project/JsonRecipeStorage.cs
+++ Project/JsonRecipeStorage.cs
@@ -1,4 +1,48 @@
+using System.Text.Json;
+
 namespace RecipeMS
 {
-    
+    public class JsonRecipeStorage : IRecipeStorage
+    {
+        public List<Recipe> Load()
+        {
+            try
+            {
+                string json = File.ReadAllText(@"recipe.json");
+
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    Console.WriteLine("Error -> kurets.");
+                    return new List<Recipe>();
+                }
+
+                List<Recipe>? recipes = JsonSerializer.Deserialize<List<Recipe>>(json);
+
+                return recipes ?? new List<Recipe>(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error -> {ex.Message}");
+                return new List<Recipe>();
+            }
+        }
+
+        public void Save(List<Recipe> list)
+        {
+            try
+            {
+                string serialized = JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(@"recipe.json", serialized);
+                Console.WriteLine("Data Saved To Json!");
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error -> {ex.Message}");
+            }
+
+
+        }
+    }
 }
