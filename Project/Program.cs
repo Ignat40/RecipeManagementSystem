2f61eb6407d2e06402dbdecb60b7ef71135e26f1
Ignat Bojinov <ignatbojinov@gmail.com>
2024-03-03 20:21:50 +0100

    test in main

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,16 +6,12 @@ namespace RecipeMS
     {
         static void Main(string[] args)
         {
-            // Create an instance of RecipeManager with JsonRecipeStorage
             var recipeManager = new RecipeManager(new JsonRecipeStorage());
 
-            // Add a test recipe
             recipeManager.AddRecipe();
 
-            // Save recipes to the JSON file
             recipeManager.Save();
 
-            // Load and display recipes
             recipeManager.Load();
             recipeManager.ViewRecipe();
         }
